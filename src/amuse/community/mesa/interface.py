5ffb92dea7733a9532d714973db37eeb217df873
Gijs Vermariën <vermarien@strw.leidenuniv.nl>
2023-09-07 15:06:20 +0200

    Errors now raise when only requested code is missing
    Before you would need both codes to be present, now
    the default behaviour is to see if the code that is
    requested is present, not the other

--- src/amuse/community/mesa/interface.py
+++ src/amuse/community/mesa/interface.py
@@ -1,32 +1,55 @@
 """
 MESA chooser
 """
-from amuse.community.mesa_r2208.interface import \
-    MESAInterface as MESAInterface_2208
-from amuse.community.mesa_r15140.interface import \
+import_successful = {"2208": False, "15140": False}
+try:
+    from amuse.community.mesa_r15140.interface import \
     MESAInterface as MESAInterface_15140
-from amuse.community.mesa_r2208.interface import MESA as MESA_2208
-from amuse.community.mesa_r15140.interface import MESA as MESA_15140
+    from amuse.community.mesa_r15140.interface import MESA as MESA_15140
+    import_successful["15140"] = True
+except ModuleNotFoundError as exception:
+    import_successful["15140"] = exception.args[0]
 
+try:
+    from amuse.community.mesa_r2208.interface import \
+        MESAInterface as MESAInterface_2208
+    from amuse.community.mesa_r2208.interface import MESA as MESA_2208
+    import_successful["2208"] = True
+except ModuleNotFoundError as exception:
+    import_successful["2208"] = exception.args[0]
+
+def check_if_mesa_imported(version):
+    # First check if the version is supported in AMUSE
+    if not str(version) in import_successful.keys():
+        raise AttributeError(
+            f"MESA version {version} is not supported by AMUSE"
+        )
+    # Then check if the correct version was imported succesfully
+    if not (import_successful[str(version)] == True):
+        raise (
+            f"MESA version {version} could not be loaded. Orginal error: {import_successful[str(version)]}"
+)
 
 def MESAInterface(version="15140", **options):
+    check_if_mesa_imported(version)    
     if str(version) == "2208":
         return MESAInterface_2208(**options)
     if str(version) == "15140":
         return MESAInterface_15140(**options)
     raise AttributeError(
-        "This version of MESA is not (yet) supported by AMUSE"
-    )
+            f"MESA version {version} is not supported by AMUSE"
+        )
 
 
 def MESA(version="15140", **options):
+    check_if_mesa_imported(version)    
     if str(version) == "2208":
         return MESA_2208(**options)
     if str(version) == "15140":
         return MESA_15140(**options)
     raise AttributeError(
-        "This version of MESA is not (yet) supported by AMUSE"
-    )
+            f"MESA version {version} is not supported by AMUSE"
+        )
 
 
 Mesa = MESA
